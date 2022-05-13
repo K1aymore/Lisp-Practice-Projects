cf4bd2a3f454f4aab9b0e9f6d0ea34fd488f0ff9
K1aymore <klaymorer@protonmail.com>
2022-05-13 13:52:17 -0700

    Add prompt for making CDs

--- cd-list.lisp
+++ cd-list.lisp
@@ -3,6 +3,20 @@
 (defun make-cd (title artist rating)
   (list :title title :artist artist :rating rating))
 
-(defun add-record (cd) 
+(defun add-cd (cd) 
   (push cd *db*))
 
+(defun print-db ()
+  (dolist (cd *db*)
+    (format t "~{~a:~10t~a~%~}~%" cd)))
+
+(defun prompt-read (prompt)
+  (format *query-io* "~a: " prompt)
+  (force-output *query-io*)
+  (read-line *query-io*))
+
+(defun prompt-make-cd ()
+  (make-cd
+    (prompt-read "Title")
+    (prompt-read "Artist")
+    (parse-integer (prompt-read "Rating") :junk-allowed t)))
